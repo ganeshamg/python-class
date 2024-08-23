caab01d1be3ad5acc18cbab11d50048671620aa1
unknown <ganesh.amgain3646@gmail.com>
2024-08-23 07:57:21 +0545

    Python Learning

---
+++ casting.py
@@ -0,0 +1,49 @@
+# a = 11
+# a = input()
+# print(a)
+
+# b = "learning python"
+# b = input ()
+# print (b)
+
+# a = input("enter a first letter")
+# b = input("enter a second letter")
+# print(a+b)
+
+
+# print(type(a))
+# print(a+b)
+
+
+a = True
+b = False
+c = True
+
+print ("or", a or b)
+print ("and", a and b)
+print ("not", not (a))
+
+
+print (a or b or c or a or c)
+print (a or b and c or a or c )
+print(a or b or c or a and c)
+
+
+x = 7
+y = 2
+print(x)
+print(type(x))
+print(type(y))
+print(x**y)
+print(x//y)
+print(x%y)
+
+
+
+
+
+
+
+
+
+
---
+++ ganesh.py
@@ -0,0 +1,43 @@
+a= 1
+b=2
+c=b-a
+print(c)
+
+c = b+a
+
+print(c)
+
+
+# **
+a= 3
+b= 2
+
+print(a**b)
+
+# floor division
+print(a//b)
+
+
+# modulus (%)
+print(a%b)
+
+
+a = 10
+b = 5
+a=10
+b-2
+print(a+b)
+
+
+
+a = "ganesh "
+b = "amgain"
+print(a+b)
+
+a = "11"
+b = "11"
+print(a+b)
+
+
+
+
