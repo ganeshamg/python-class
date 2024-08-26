f2bc33181b1d3230a28e5ee5bd4e9a31176618d7
unknown <ganesh.amgain3646@gmail.com>
2024-08-26 08:02:07 +0545

    pratice class

---
+++ ifcondition.py
@@ -0,0 +1,54 @@
+
+
+# syntax
+
+# '''
+# if (condition):
+#     code block
+
+
+#     '''
+
+# if (2==3):
+#     print("this is true")
+
+# a = 2
+# b = 2
+# if (2==2):
+#     print("this is true")
+    
+
+# if True:
+#     print("thsi is from True")
+
+# a=2
+# b=3
+
+# if (a==b):
+#     print("this True")
+# else:
+#     Print("this is false")
+
+    # input
+    # typecasting
+    # Arthamatic Operator
+    # If Condition
+    # Conditional Operator
+
+# a = input('enter a number: ')
+# b=int(a)
+
+# if (b%2==0):
+#     print("even")
+# else:
+#     print('odd')
+   
+if (2==2):
+    print("if condition is true")
+elif(3==3):
+    print("elif condition isn true")
+else:
+    print("else condition")
+
+
+
---
+++ log.py
@@ -0,0 +1,58 @@
+# logical operator TRUE OR FALSE
+
+# AND OR NOT
+
+# A AND B BOTH TRUE / TRUE AND TRUE IS True
+# OR IF ONE CONDITION IS TRUE THEN RESULT IS True
+
+# NOT OPPOSITE RESULT
+
+# NOT TRUE False
+
+# CONDITIONAL OPERATOR
+# greater than lower than equalls to 
+# answer alwyas in true and false
+
+a = 2
+b = 3
+
+# equal to
+
+# = asign
+
+# == value compare
+
+print (a==b)
+
+# notequal to
+# !=
+
+#greater than
+# >
+#less than
+# <
+
+a = 3
+b = 2
+print (a==b)
+print (a != b)
+print (a>b)
+print (a<b)
+
+# less than equal to
+# <=
+print (a<=b)
+
+# greater than equal to
+# >=
+print (a>=b)
+
+
+
+
+
+
+
+
+
+
