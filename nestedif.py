e83b469c2e8f34411c47b3b06ba3e425a9d98556
unknown <ganesh.amgain3646@gmail.com>
2024-08-27 07:33:40 +0545

    study work

---
+++ nestedif.py
@@ -0,0 +1,24 @@
+# nested if
+# percentage = 55
+
+# if(percentage>=40):
+#     print("user is pass")
+#     if(percentage>=80):
+#         print("user in distinction")
+#     elif(percentage>=60):
+#         print(user in first dicision)
+# else:
+#     Print("fail")
+
+
+         
+# singleline if
+# gender = "M"
+# if (gender=="M"):
+#  print("male")
+# else:
+#    print("female")
+
+# gender = "M"
+# a = "Male" if gender=="M" else "Female"
+# print(a)
