bc5742de7888fc6d92d7e4440e9628177403e987
unknown <ganesh.amgain3646@gmail.com>
2024-09-08 18:35:22 +0545

    homework

---
+++ games.py
@@ -0,0 +1,43 @@
+# import random
+
+# a = random.random()
+# print(int(a*100))
+
+# b = random.randrange(100,105)
+# print(b)
+
+# import random
+# num = random.randrange(1,15)
+# print(num)
+# i = 0
+
+# while (i<4):
+#     i = i+1
+#     user_num = int(input("guess the number: "))
+#     if (num == user_num):
+#         print("congratuation, correct guess")
+#         break
+#     if(i==3):
+#         print("guess, limit exceeded")
+#     print("incorrect guess, try again")
+
+#  # findout inwhich try you reached at correct guess
+
+# import random
+# num = random.randrange(1,15)
+# print(num)
+# i = 0
+
+# while (i<4):
+#     i = i+1
+#     user_num = int(input("guess the number: "))
+#     if (num == user_num):
+#         print("congratuation, correct guess")
+#         break
+#     if(i==3):
+#         print("guess, limit exceeded")
+#     print("incorrect guess, try again")
+
+
+
+
--- homework.py
+++ homework.py
@@ -17,9 +17,48 @@ bablu = {}
 # for i in range(1, 11):
 #     print(f"5 x {i} = {5*i}")
 
-n = int(input("enter a number"))
-for i in range(1,11):
-    print(f"{n} x {i} = {n*i}")2,3,5
+# n = int(input("enter a number"))
+# for i in range(1,11):
+#     print(f"{n} x {i} = {n*i}")2,3,5
+
+# for loop
+# n = 5 # number of row
+# for i in range(1, n+1):
+#     for j in range(1, i+1):
+#         print(j, end=" ")
+#     print()
+
+# while loop
+# n = 5 # number of row
+# i = 1
+# while i <= n:
+#     j = 1
+#     while j <= i:
+#         print(j , end=" ")
+#         j += 1
+#         print()
+#         if i == 3: # break after the third row
+#             break
+#         i += 1
+
+# # accept a number from the user
+# num = int(input("enter a number: "))
+# # initialize sum to 0
+# total_sum = 0
+# # loop to calculate the sum of all numbers from 1 to the given number
+# for i in range(1, num+1):
+#     total_sum += i
+# # print the results
+# print(f"the sum of all numbers from 1 to {num} is {total_sum}")
 
 
+# # first half of the pattern
+# n = 5 # number of rows
+# for i in range (n, 0, -1):
+#     print('*' * i)
+
+# # second half of the pattern
+# for i in range(2, n+1):
+#     print('*' * i)
+    
 
